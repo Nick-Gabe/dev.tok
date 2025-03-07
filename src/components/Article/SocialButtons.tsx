b365813acab34ec8d4f19b1b508c19b69e1c131d
Joao Astolfi <jpastolfi@gmail.com>
2025-03-07 12:11:26 +0000

    Modal para dropdown, share, redirect

--- src/components/Article/SocialButtons.tsx
+++ src/components/Article/SocialButtons.tsx
@@ -9,11 +9,6 @@ const copyToClipboard = (article: Article) => {
   navigator.clipboard.writeText(article.url)
 }
 
-const reportWithReferrer = (article: Article) => {
-  window.open(`https://dev.to/report-abuse?url=${encodeURIComponent(article.url)}`, '_blank');
-}
-
-
 export const SocialButtons = ({
   article,
   onCommentsClick,
@@ -51,27 +46,13 @@ export const SocialButtons = ({
         </button>
         <p className="text-gray-100 text-sm">{article.comments_count}</p>
       </div>
-      <div className="flex flex-col text-center gap-1 w-10">
-        <button onClick={() => {
-          const modal = document.getElementById('my_modal_1') as HTMLDialogElement;
-          modal.showModal();
-        }}>
-          <DotsThreeOutline className="text-gray-100 w-10 h-10" weight="fill" />
-        </button>
-        <dialog id="my_modal_1" className="modal">
-          <div className="modal-box">
-            <div className="flex flex-col w-1/2 items-center justify-center m-auto">
-              <button className="btn bg-accent m-2" onClick={() => copyToClipboard(article)}>Copy link</button>
-              <button className="btn bg-accent m-2 block md:hidden" onClick={() => navigator.share({ title: article.title, url: article.url })}>Share to</button>
-              <button className="btn bg-error m-2" onClick={() => reportWithReferrer(article)}>Report</button>
-            </div>
-          <div className="modal-action">
-            <form method="dialog">
-              <button className="btn">Close</button>
-            </form>
-          </div>
-        </div>
-      </dialog>
+      <div className="dropdown dropdown-top dropdown-end">
+        <div tabIndex={0} role="button"><DotsThreeOutline className="text-gray-100 w-10 h-10" weight="fill" /></div>
+        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li><button className="btn bg-accent m-2" onClick={() => copyToClipboard(article)}>Copy link</button></li>
+          <li><button className={`btn bg-accent m-2 block ${typeof navigator.share === 'function' ? '' : 'hidden'}`} onClick={() => navigator.share({ title: article.title, url: article.url })}>Share to</button></li>
+          <li><a className="btn bg-error m-2" href={`https://dev.to/report-abuse?url=${encodeURIComponent(article.url)}`} target="_blank" rel="noopener noreferrer">Report</a></li>
+        </ul>
       </div>
     </div>
   );
